fdf994ffca2ddb7853d7866336f31be5679233e0
hongphuc121299 <hongphuc121299@gmail.com>
2020-11-20 16:34:49 +0700

    add JUnit and libs and Test Cases

--- src/phuc/util/MathUtility.java
+++ src/phuc/util/MathUtility.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -7,81 +8,81 @@ package phuc.util;
 
 /**
  *
- * @author ADMIN
+ * @author giao-lang
  */
-public class MathUtility {
-    //Mình sẽ làm class này fake y chang calsss Math của JDK 
-    // với các hàm static để xài chung cho mọi nơi 
+//ta sẽ làm class fake class Math với các hàm static để xài chung cho mọi nơi
+    public class MathUtility {
+    
+    //ta sẽ làm class fake class Math với các hàm static để xài chung cho mọi nơi
     public static final double PI = 3.14;
-    //hàm tính n! = 1.2.3..n
-    //trong đó khong giai thừa âm, >20 tràn kiểu long
-    //quy ước 0! = 1! = 1
-    //Nếu đưa vào n âm, hoặc n>20, ta khong thèm tính
-    public static  long getFactorial(int n){
-        if(n<0 || n>20)
+    
+    //hàm tính n! = 1.2.3...n
+    //trong đó ko giai thừa âm, > 20 tràn kiểu long
+    //quy ước 0! = 1! = 1;
+    //đưa vào n âm, hoặc n > 20, ta ko thèm tính gì cả, chửi Exception
+    public static long getFactorial(int n) {
+        if (n < 0 || n > 20)
             throw new IllegalArgumentException("n must be between 0..20");
         
-        if(n==0 || n==1)
-            return 1; //1 lệnh trong if không sài ngoặc
-        //chỗ này tk nào sài else trừ điểm
-        //ngay chỗ nay chinh là n = 2..20
+        if (n == 0 || n == 1)
+            return 1; //1 lệnh trong if ko xài ngoặc
+        
+        //chỗ này thằng nào xài else trừ điểm
+        //ngay chỗ này chính là n = 2..20
         long result = 1;
-        for (int i = 2; i <= n; i++) {
+        
+        for (int i = 2; i <= n; i++) 
             result *= i;
-        }
+        
         return result;
     }
-    
-    //ta tự tin gáy hàm ta chạy ngon, chạy đúng
-    //nhưng: team thì cần chứng minh điểu đó, bảo đảm chất lượng code, chất lượng hàm
-    //muốn chứng minh hàm chạy đúng hay không ta cần test thử
-    //ví dụ gọi hàm getFactorial(với các số khác nhau đưa vào)
-    //getF(-5), getF(-1), getF(0), getF(5).....
-    //các giá trị đầu vào đại diện cho vc sài hàm
-    //ta còn gọi là test case, các tình huống cần test, các tình huống xài hàm
-    //khi ta test hàm, ta cần quan tâm 2 vc
-    //1. dự kiến hàm trả về giá trị gì khi hàm chạy với 1 đàu vào nào đó
-    //   dự kiến hàm trả về giá trị, tui gọi là EXPECTED VALUE
-    //   ví dụ : hàm sẽ trả về giá trị 120 khi gọi getF(5)
-    //2. hàm khi chạy với 1 đầu vào nào đó, thì nó sẽ ói/return về 1 value
-    
-    //  NẾU TEST CHÍNH LÀ: SO SÁNH CÁI EXPECTED VALUE CÓ BẰNG VỚI ACTUAL VALUE HEM??
-    //  NẾU CÓ BẰNG, HÀM ĐÚNG CHO CASE, TÌNH HUỐNG NÀY
-    //  NẾU KO BẰNG, HÀM TÍNH TOÁN SAI, KÌ VỌNG SAI
-    
-    //HÀM MÀ TỐT THÌ PHẢI LÀ EXPECTED VALUE == ACTUAL, MỌI TÌNH HUỐNG
-    //NẾU TỐT TA MỚI ĐEM RA XÀI, BÁN, PUBLIC CỘNG ĐỒNG
-    
-    //LÀM SAO ĐỂ TEST HÀM COI CÓ TỐT KHÔNG, EXPECTED  == ACTUAL
-    //HAI KĨ THUẬT ĐƯỢC ÁP DỤNG
-    //1. NHÌN BẰNG MATWSVAS SO SÁNH TỪNG CẶP EXPECTED VS ACTUAL
-    //   KĨ THUẬT NÀY ĐƠN GIẢN, DỄ LÀM, NHƯNG MẮC SAI SÓT DO CÓ QUÁ NHIỀU CẶP
-    //   EXPECTED ACTUAL CẦN SO SÁNH BẰNG MẮT
-    //KĨ THUẬT 2: nhìn bằng mắt, không cần so sánh từng cặp
-    public static void main(String[] args) {
-        //case 1:
-        long expected = 120; //tao hi vọng 120 ói về nếu
-        long actual = MathUtility.getFactorial(5); //tao gọi hàm 5!
-        System.out.println("5!!? expected: " + expected+ "; actual: " + actual);
-        //đúng cho case đầu tiên
-        
-        //case 2:
-        expected = 720; //tao hi vọng 720 ói về nếu
-        actual = MathUtility.getFactorial(6); //tao gọi hàm 6!
-        System.out.println("6!!? expected: " + expected+ "; actual: " + actual);
-        //đúng cho case 2
-        
-        //case 3
-        System.out.println("0!!? expected: 1" + expected+ "; actual: " + MathUtility.getFactorial(0));
-        
-        //case 4:
-        //tao kì vọng nhận về IllegalArgumentException
-        //nếu t gọi -5!
-        MathUtility.getFactorial(-5);
-        //nhớ link kho 
-        //cần file đặc biệt để nói với git tool rằng ai lên sever ai ở lại local file này được gọi là .gitignore.txt thuần text khác nhau content tùy vào IDE mình xài và 
-        //ngôn ngữ lập trình mình xài 
-        //Có một gã dev cực kì dễ thương đã làm snawx nhugnwx file này ròi ứng với các IDE và các ngôn ngữ lapapj trình mình chỉ xin anh ấy đem về xài 
-        //https://gitignore.io
-    }
 }
+
+//ta tự tin gáy hàm ta chạy ngon, chạy đúng
+//nhưng: team thì cần chứng minh điều đó, đảm bảo chất lượng code, chất lượng hàm
+//muốn chứng minh hàm chạy đúng hay ko ta cần test thử
+//test nghĩa là đưa hàm ra xài, gọi với các tham số khác nhau
+//ví dụ gọi hàm getFactorial(với các số khác nhau đưa vào)
+//getF(-5), getF(-1) getF(0), getF(5), getF(20), getF(21), ...
+//các giá trị đầu vào đại diện cho việc xài hàm,
+//ta còn gọi là test cases, các tình huống cần test, các tình huống xài hàm
+//khi ta test hàm, ta cần quan tâm 2 việc
+//1. dự kiến hàm trả về giá trị gì khi hàm chạy với 1 đầu vào nào đó
+//   dự kiến hàm trả về giá trị, tui gọi là EXPECTED VALUE
+//   ví dụ: hàm sẽ trả về 120 khi gọi getF(5!)
+//2. hàm khi chạy với 1 đầu vào nào đó, thì nó sẽ ói/return về 1 value
+//   cái value trả về của hàm khi chạy đc gọi là ACTUAL VALUE
+
+//NGHỀ TEST CHÍNH LÀ: SO SÁNH COI EXPECTED VALUE CÓ BẰNG VỚI ACTUAL VALUE HEM?
+//NẾU CÓ BẰNG, HÀM ĐÚNG CHO CASE/TÌNH HUỐNG NÀY
+//NẾU KO BẰNG, HÀM TÍNH TOÁN SAI, HOẶC KÌ VỌNG SAI
+
+//HÀM MÀ TỐT THÌ PHẢI LÀ EXPECTED == ACTUAL MỌI TÌNH HUỐNG
+//NẾU TỐT TA MỚI ĐEM RA XÀI, BÁN, PUBLIC CỘNG ĐỒNG XÀI
+
+//LÀM SAO ĐỂ TEST HÀM COI CÓ TỐT KO, EXPECTED == ACTUAL
+//HAI KĨ THUẬT ĐC ÁP DỤNG
+//KĨ THUẬT 1: Nhìn bằng mắt và so sánh từng cặp expected vs. actual
+//          chính là kĩ thuật sout(expected), sout(actual-hàm ói về gì khi chạy)
+//kĩ thuật này đơn giản, dễ làm, nhưng mắc sai sót do có quá nhiều cặp
+//expected actual cần so sánh bằng mắt
+
+//KĨ THUẬT 2: Nhìn bằng mắt, ko cần so sánh từng cặp, để máy so sánh giùm luôn
+//           mắt chỉ nhìn 2 màu XANH-ĐỎ DUY NHẤT
+//nếu tất cả các cặp actual vs. expected đều thỏa, MÀU XANH CHO TẤT CẢ
+//nếu HẦU HẾT các cặp actual vs. expected đều thỏa, có 1 vài cái ko thỏa
+//THÌ KẾT LUẬN NGÀY, HÀM SAI, MÀU ĐỎ CHO TẤT CẢ
+//điều này giải thích là: đúng thì phải đúng cho tất cả mọi tình huống hàm chạy
+//chỉ cần 1 thằng sai, hàm sai
+
+//tương đương câu: hàm tui chạy ngon lắm, đúng 99.9%, lâu lâu sai tí
+//nói câu đó: hàm éo tin cậy để xài
+//KĨ THUẬT 2 NÀY DÙNG MÀU SẮC, MÚN LÀM VẬY THÌ PHẢI XÀI THƯ VIỆN BỔ SUNG THÊM
+//CHÍNH LÀ FILE .JAR, .DLL ĐC CUNG CẤP THÊM NGOÀI JDK
+//Các thư viện này tùy thuộc ngôn ngữ lập trình, đc gọi chung là 
+//Unit Test framework, 
+//C#: thư viện cụ thể NUnit, MSUnit, xUnit
+//Java: thư viện cụ thể JUnit, TestNG, xUnit
+//PHP:  PHPUnit
+//C++: CPPUnit
+//...
---
+++ test/fu/util/test/MathUltilityTest.java
@@ -0,0 +1,46 @@
+package fu.util.test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.sql.SQLException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import phuc.util.MathUtility;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class MathUltilityTest {
+    @Test
+    // biến cái hàm ở trong class này thành public static void main nhờ bộ thư viện JUnit
+    // Vì mặc định app chạy từ main chuẩn trong khi đó mình cần test thử hàm thôi không can thiệp main vì main dành cho app chạy vậy mình cần main khác và @Test giúp điều đó 
+    //Nhưng vì có nhiều main nên CPU bị lỗi khi nhấn F6 như v F6 dành cho main mặc định còn shift F6 dành cho bên này
+    
+    //Cú pháp đặt tên hàm dùng để test hàm khác cuar dân QC 
+    public void getFactorial_RunWell_IfValidArgument() {
+        assertEquals(120, MathUtility.getFactorial(5));
+        assertEquals(720, MathUtility.getFactorial(6));
+        assertEquals(24, MathUtility.getFactorial(4));
+        assertEquals(6, MathUtility.getFactorial(3));
+        assertEquals(1, MathUtility.getFactorial(0));
+    }
+    //hyperlink nhỏ nhỏ kệ nó luôn 
+    
+    //bắt ngoại lệ thế nào
+    //ngoại lệ không phải là một value để mà so sánh do đó dùng hàm assertEquals là không được 
+    //ta áp dụng kỹ thuật khác
+    
+    @Test(expected = SQLException.class)
+    public void getFactorial_ThrowsException_IfInvalidArgument() {
+        MathUtility.getFactorial(-5);
+    }
+}
